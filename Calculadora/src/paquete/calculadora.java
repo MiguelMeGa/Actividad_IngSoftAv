57a84527042b4f8798130b0d543a94ccd388807b
MiguelMeGa <mendezgayol93@gmail.com>
2022-05-28 12:19:19 +0200

    Implementacion suma

--- Calculadora/src/paquete/calculadora.java
+++ Calculadora/src/paquete/calculadora.java
@@ -2,4 +2,16 @@ package paquete;
 
 public class calculadora {
 
+	public calculadora()
+	{
+		
+		
+	}
+	
+	public double suma (double a, double b)
+	{
+		
+		return a+b;//Se devuelve la suma de a +b
+	}
+	
 }
