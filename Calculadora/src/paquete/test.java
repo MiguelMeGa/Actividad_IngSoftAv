2e515c89f01bd2cc9cec133781d1036d40075491
MiguelMeGa <mendezgayol93@gmail.com>
2022-05-28 16:47:19 +0200

    Mejora del test de suma

--- Calculadora/src/paquete/test.java
+++ Calculadora/src/paquete/test.java
@@ -1,24 +1,59 @@
 package paquete;
-
-import static org.junit.Assert.*;
-
-import org.junit.Test;
+import java.text.DecimalFormat;
 
 public class test {
-
-	@Test
-	public void test() {
-		
-		//PAra el test  unitario
-		fail("Not yet implemented");
 	
-	}
 	
-    
-	public void compruebavalores(double a, double b)
+	public test()
 	{
 		
-		assertEquals(a,b,0.001); //Precisión de 10-3
+		
 	}
+	private static final DecimalFormat df = new DecimalFormat("0.000");
+	
+	public void  compruebavalores(double a, double b)
+	{
+		double x,y;
+		x=formato3decimales(a);
+		y=formato3decimales(b);
+		
+		if(x!=y)
+		{    
+			System.out.println( "Error " +x+ " distinto de " +y);
+		}
+		else
+		{
+			
+		}
+	   
+	}
+	
+	private double formato3decimales(double num)
+	{
+		double numero,decimal,vuelta;
+		int entero;
+		
+		
+		numero=num*1000;       //Se toma el número en cuestíón
+		entero=(int)(numero);  //se toma la parte entera
+		
+		decimal=numero-entero; //Nos quedamos con la parte decimal
+		
+		
+		if(decimal>0.5)        //Si la parte decimal es mayor que 0,5
+		{
+			
+			entero=entero+1; //Se suma 1 a la parte entera
+			
+		}
+		//En caso contrario uno se queda donde está
+		
+		vuelta=(double)(entero/1000);
+		
+		return vuelta; 
+
+	}
+	
+  
 
 }
