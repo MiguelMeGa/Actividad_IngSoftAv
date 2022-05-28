2ec59b6dc0f128d8661330a0588d686e4a4d3f3b
MiguelMeGa <mendezgayol93@gmail.com>
2022-05-28 17:13:35 +0200

    Implementacion resta

--- Calculadora/src/paquete/calculadora.java
+++ Calculadora/src/paquete/calculadora.java
@@ -8,10 +8,25 @@ public class calculadora {
 		
 	}
 	
+	////////////////////////////////////////
+	//             Método de la suma
+	////////////////////////////////////////
+	
 	public double suma (double a, double b)
 	{
 		
 		return a+b;//Se devuelve la suma de a +b
 	}
+	
+	//////////////////////////////////////////
+	// 			Método para la resta
+	/////////////////////////////////////////
+	
+	public double resta(double a, double b)
+	{
+		return a-b;
+	}
+	
+	
 	
 }
